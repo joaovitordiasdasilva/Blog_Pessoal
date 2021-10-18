33e5a87894763291265b84e5ccf1bf1eabe805c8
João vitor <joaovitordiasdasilva1@uni9.edu.br>
2021-10-18 11:37:02 -0300

    Modificado BasicSecurity

--- src/main/java/org/generation/blodPessoal/security/BasicSecurityConfig.java
+++ src/main/java/org/generation/blodPessoal/security/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package org.generation.blodPessoal.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,26 +16,25 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	private UserDetailsService userDetailService;
+	private UserDetailsService userDetailsService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailService);
-
-		auth.inMemoryAuthentication().withUser("root").password(passwordEnconder().encode("root"))
-				.authorities("ROLE_ADMIN");
+		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication().withUser("root").password(passwordEncoder().encode("root"))
+				.authorities("ROLE_USER");
 	}
 
 	@Bean
-	public PasswordEncoder passwordEnconder() {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/usuario/logar").permitAll().antMatchers("/usuario/cadastrar").permitAll()
-				.anyRequest().authenticated().and().httpBasic().and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and().csrf().disable();
-
+		http.authorizeRequests().antMatchers("/usuarios/cadastrar").permitAll().antMatchers("/usuarios/logar")
+				.permitAll().antMatchers(HttpMethod.OPTIONS).permitAll().anyRequest().authenticated().and().httpBasic()
+				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().cors().and()
+				.csrf().disable();
 	}
 }
